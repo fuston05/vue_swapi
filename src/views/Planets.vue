8dbb002ea84a8bdaba8cc782c4182d4688717a4e
scott fuston <fuston05@yahoo.com>
2021-10-09 18:09:04 -0400

    removes commented code

--- src/views/Planets.vue
+++ src/views/Planets.vue
@@ -73,44 +73,6 @@ export default {
 
       this.isLoading = false;
     }
-
-    // async getPlanets(reqPage = "") {
-    //   if (this.isLoading || reqPage === null) return;
-
-    //   this.isLoading = true;
-
-    //   const res = await fetch(
-    //     `${process.env.VUE_APP_API}/planets/?page=${reqPage}`
-    //   );
-
-    //   const data = await res.json();
-
-    //   this.planets = data.results;
-
-    //   this.perPage = this.perPage ? this.perPage : data.results.length;
-
-    //   this.page = reqPage ? parseInt(reqPage) : 1;
-
-    //   // calc total pages
-    //   this.totalPages = Math.floor((data.count - 1) / this.perPage + 1);
-
-    //   // page calculation
-    //   this.nextPage = data.next
-    //     ? data.next
-    //         .split("/")
-    //         .splice(-1)[0]
-    //         .slice(-1)
-    //     : null;
-
-    //   this.prevPage = data.previous
-    //     ? data.previous
-    //         .split("/")
-    //         .splice(-1)[0]
-    //         .slice(-1)
-    //     : null;
-
-    //   this.isLoading = false;
-    // }
   },
 
   created() {
