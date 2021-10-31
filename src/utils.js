2bea7c0b8ebcb61d42ec57ee92e1f0d6eaa0ff5a
scott fuston <fuston05@yahoo.com>
2021-10-31 10:18:46 -0400

    removes console.log

--- src/utils.js
+++ src/utils.js
@@ -27,7 +27,6 @@ export async function fetchData(reqPage, pageData, resName) {
     data.count && sessionStorage.setItem(resName, JSON.stringify(data));
   }
 
-  console.log({ data });
   const _resource = data.results;
 
   // items per page
